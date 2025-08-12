be778986f146d1a69bbe4e721c662fb981b02341
KhoaTran160401 <tranvankhoa.ag.01@gmail.com>
2025-08-12 14:10:44 +0700

    fix logic send message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -99,7 +99,7 @@ module.exports.sendMessage = async (req, res) => {
         const receiverSocketId = getReceiverSocketId(receiverId);
         if (receiverSocketId) {
             io.to(receiverSocketId).emit("newMessage", customNewMessage);
-            io.emit("newMessage", customNewMessage);
+            //io.emit("newMessage", customNewMessage);
         }
 
 
@@ -159,7 +159,7 @@ module.exports.sendMessageWithImage = async (req, res) => {
         const receiverSocketId = getReceiverSocketId(receiverId);
         if (receiverSocketId) {
             io.to(receiverSocketId).emit("newMessage", customNewMessage);
-            io.emit("newMessage", customNewMessage);
+            //io.emit("newMessage", customNewMessage);
         }
 
         response.data = newMessage;
